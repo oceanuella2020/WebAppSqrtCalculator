bf20d42e6897ad9d17c1fec8c0fc6d1e07cd2707
Emmanuella Fatolu <61954156+oceanuella2020@users.noreply.github.com>
2020-05-04 22:13:08 +0100

    Initial commit

---
+++ Controllers/CalcuController.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppSqrtCalculator.Controllers
+{
+    public class CalcuController:Controller
+    {
+        [HttpGet]
+        public ActionResult Sqrt()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Sqrt(string firstNumber,string secondNumber)
+        {
+            int numberOne=int.Parse(firstNumber);
+            int numberTwo=int.Parse(secondNumber);
+            double result=Math.Sqrt(numberOne);
+            double result2=Math.Sqrt(numberTwo);
+            ViewBag.result=result;
+            ViewBag.result2=result2;
+            ViewBag.numberOne=firstNumber;
+            ViewBag.numberTwo=secondNumber;
+            return View();
+
+        }
+    }
+}
